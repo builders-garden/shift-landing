bf4d338874a2f96abe36b41d1c78ac9d46fd6b9c
lim0n3 <simonestaffa96@gmail.com>
2024-08-20 15:49:03 +0100

    add preview image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Overpass } from "next/font/google";
 import "./globals.css";
+import { BASE_URL } from "@/lib/utils";
 
 const overpass = Overpass({ subsets: ["latin"] });
 
@@ -8,6 +9,19 @@ export const metadata: Metadata = {
   title: "Shift - POS for Merchants",
   description:
     "Innovative POS for merchants: lower fees, higher margins, loyal customers",
+  openGraph: {
+    title: "Shift - POS for Merchants",
+    description:
+      "Innovative POS for merchants: lower fees, higher margins, loyal customers",
+    images: [
+      {
+        url: `${BASE_URL}/images/preview.png`,
+        width: 1200,
+        height: 630,
+        alt: "Shift - POS for Merchants",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
---
+++ lib/utils.ts
@@ -0,0 +1,2 @@
+export const BASE_URL =
+  process.env.BASE_URL || process.env.VERCEL_URL || "http://localhost:3000";
